e29c87033f2dc820f4c524760b4df8608cebcf4b
Abhey Rana <abheyranacool123.ar@gmail.com>
2018-03-02 14:56:04 +0530

    Fixed bug in Todo list

--- bot/todo.py
+++ bot/todo.py
@@ -1,5 +1,6 @@
-import sys, os
-sys.path.insert(0,os.getcwd())
+import sys
+import os
+sys.path.insert(0, os.getcwd())
 
 import copy
 import importlib
@@ -23,35 +24,36 @@ class todo(object):
 handler_class = todo
 '''
 
+
 def get_todo_response(message: Dict[str, str], bot_handler: Any) -> str:
-	content = message['content']
-	words = content.split()
-	if words[2] == "start" :
-		bot_handler.storage.put("list" , "")
-		return "todo initialized"
-	if words[2] == "list" :
-		res = bot_handler.storage.get("list") 
-		val = ""
-		values = res.split("~")
-		i = 1  
-		for temp in values :
-			val = val + str( i ) + ". " + temp + "\n"
-			i += 1 
-		return val 
-	elif words[2] == "add" :
-		res = bot_handler.storage.get("list")
-		res = res + words[3] + "~" 
-		bot_handler.storage.put( "list" , res )
-		return "Added to list."
-	elif words[2] == "remove" :
-		index = int( words[3] ) 
-		res = bot_handler.storage.get("list") 
-		val = ""
-		values = res.split("~") 
-		i = 1
-		for temp in values :
-			if i != index :
-				val = val + temp + "~"
-			i += 1 
-		bot_handler.storage.put( "list" , val )
-		return "Removed from list."
+    content = message["content"]
+    words = content.split()
+    if words[2] == "start":
+        bot_handler.storage.put("list", "")
+        return "todo initialized"
+    if words[2] == "list":
+        res = bot_handler.storage.get("list")
+        val = ""
+        values = res.split("~")
+        i = 1
+        for temp in values:
+            val = val + str(i) + ". " + temp + "\n"
+            i += 1
+        return val
+    elif words[2] == "add":
+        res = bot_handler.storage.get("list")
+        res = res + " ".join(words[3::]) + "~"
+        bot_handler.storage.put("list", res)
+        return "Added to list."
+    elif words[2] == "remove":
+        index = int(words[3])
+        res = bot_handler.storage.get("list")
+        val = ""
+        values = res.split("~")
+        i = 1
+        for temp in values:
+            if i != index:
+                val = val + temp + "~"
+            i += 1
+        bot_handler.storage.put("list", val)
+        return "Removed from list."
